08c14ae074cf53ddc05179dde5d926c35281959d
Vlad-Eris <vlad.mihai@emanuel.ro>
2024-01-19 09:34:51 +0200

    SistemAutentificare

---
+++ Sistem-autentificare-folosind-bucle.git/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+class Program
+{
+    const string UserValid = "utilizator";
+    const string ParolaValida = "parola123";
+
+    static void Main()
+    {
+        while (true)
+        {
+            Console.Write("Introduceti username: ");
+            string username = Console.ReadLine();
+
+            Console.Write("Introduceti parola: ");
+            string parola = Console.ReadLine();
+
+            if (Autentificare(username, parola))
+            {
+                Console.WriteLine($"Bine ai venit, {username}!");
+            }
+            else
+            {
+                Console.WriteLine("Autentificare esuata. Incercati din nou.");
+            }
+        }
+    }
+
+    static bool Autentificare(string username, string parola)
+    {
+        return username == UserValid && parola == ParolaValida;
+    }
+}
